508c2e7091b1b87547cbc43aa5bca5a888cd5b74
VitaliiDevX <vitalii.dev.x@gmail.com>
2026-01-21 19:04:54 +0200

    create welcome page, authNav and allUsersTab components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import AuthProvider from "@/components/AuthProvider/AuthProvider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
+  weight: ["400", "500", "700"],
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: "--font-inter",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -25,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${inter.variable}`}>
         <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
--- components/AuthNav/AuthNav.tsx
+++ components/AuthNav/AuthNav.tsx
@@ -1,3 +1,20 @@
+import Link from "next/link";
+import css from "./AuthNav.module.css";
+
 export default function AuthNav() {
-  return <h1>AuthNav</h1>;
+  return (
+    <ul className={css.navigationList}>
+      <li className={css.signUpBtn}>
+        <Link href="/sign-up" prefetch={false} className={css.navigationLink}>
+          Sign Up
+        </Link>
+      </li>
+
+      <li className={css.signInBtn}>
+        <Link href="/sign-in" prefetch={false} className={css.navigationLink}>
+          Sign In
+        </Link>
+      </li>
+    </ul>
+  );
 }
