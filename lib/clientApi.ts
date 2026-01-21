647952bf9b1e0bd385ef48fd2807568062a9aa6b
Rostislav <operkaka4@gmail.com>
2026-01-21 15:08:13 +0300

    add api categories and auth

--- lib/clientApi.ts
+++ lib/clientApi.ts
@@ -1,10 +1,49 @@
-import { Category } from "@/type/categoty";
+import { Category, Income, Expenses } from "@/type/categoty";
 import { apiNext } from "./api";
+import { User } from "@/type/user";
 
 type UserCategory = {
   type: string;
   categoryName: string;
 };
+
+type UpdateCategory = {
+  _id: string;
+  categoryName: string;
+};
+
+type RegisterRequest = {
+  name: string;
+  email: string;
+  password: string;
+};
+
+type LoginRequest = {
+  email: string;
+  password: string;
+};
+
+type UserInfo = {
+  _id: string;
+  email: string;
+  name: string;
+  avatarUrl: string | null;
+  currency: string;
+  categories: {
+    incomes: Income[];
+    expenses: Expenses[];
+  };
+  transactionsTotal: {
+    incomes: number;
+    expenses: number;
+  };
+};
+
+type SessionResponse = {
+  success: boolean;
+};
+
+//Category
 
 export async function getCategories(): Promise<Category> {
   const res = await apiNext.get<Category>("/categories");
@@ -12,8 +51,50 @@ export async function getCategories(): Promise<Category> {
   return res.data;
 }
 
-export async function createCategories(
+export async function createCategory(
   userCategory: UserCategory,
 ): Promise<void> {
   await apiNext.post<void>("/categories", userCategory);
 }
+
+export async function deleteCategory(id: string): Promise<void> {
+  await apiNext.delete<void>(`/categories${id}`);
+}
+
+export async function updateCatecory(
+  userCategory: UpdateCategory,
+): Promise<void> {
+  await apiNext.patch<void>(`/categories/${userCategory._id}`, userCategory);
+}
+
+//Auth
+
+export async function userRegister(body: RegisterRequest): Promise<User> {
+  const { data } = await apiNext.post<User>("/auth/register", body);
+
+  return data;
+}
+
+export async function userLogin(body: LoginRequest): Promise<UserInfo> {
+  const { data } = await apiNext.post<UserInfo>("/auth/login", body);
+
+  return data;
+}
+
+export async function userLogout(): Promise<void> {
+  await apiNext.post<void>("/auth/logout");
+}
+
+export async function checkSession(): Promise<boolean> {
+  const res = await apiNext.get<SessionResponse>("/auth/session");
+
+  return res.data.success;
+}
+
+//UserInfo
+
+// export async function getUser(): Promise<UserInfo> {
+//     const res = await apiNext.get<UserInfo>("/users/current");
+
+//     return res
+// }
