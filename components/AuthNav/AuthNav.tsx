0429b3f67f8713592457b2f50e38c60035c4dcb3
Rostislav <operkaka4@gmail.com>
2026-01-21 18:34:03 +0300

    add AuthNav component

--- components/AuthNav/AuthNav.tsx
+++ components/AuthNav/AuthNav.tsx
@@ -1,3 +1,18 @@
+import Link from "next/link";
+
 export default function AuthNav() {
-  return <h1>AuthNav</h1>;
+  return (
+    <ul className="#">
+      <li key={"sign-up"} className="#">
+        <Link href="/sign-up" className="#">
+          Sign Up
+        </Link>
+      </li>
+      <li key={"sign-in"} className="#">
+        <Link href="/sign-in" className="#">
+          Sign In
+        </Link>
+      </li>
+    </ul>
+  );
 }
