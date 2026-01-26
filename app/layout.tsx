7fcc676235c49c30b98568d7bf3828ab8143e5c9
Olesia Tsaturian <olesiases@gmail.com>
2026-01-26 12:28:28 +0200

    add layout.tsx in auth routes

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.variable}`}>
-         <Toaster
+        <Toaster
           position="bottom-right"
           toastOptions={{
             duration: 13000,
