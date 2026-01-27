56ef84ff88d61b1bbc90b1f21da1ae0897015f6f
Buinov Vadym <karabincom@gmail.com>
2026-01-27 23:15:40 +0200

    Styling the transactions-history page, adding animation to the header

--- app/transactions/history/[type]/page.tsx
+++ app/transactions/history/[type]/page.tsx
@@ -1,10 +1,10 @@
 import TransactionsList from "@/components/TransactionsList/TransactionsList";
 import TransactionsSearchTools from "@/components/TransactionsSearchTools/TransactionsSearchTools";
 import { Suspense } from "react";
-// import TransactionsTotalAmount from "@/components/TransactionsTotalAmount/TransactionsTotalAmount";
-import css from "@/app/transactions/history/[type]/page.module.css";
 import { getTransactionByType } from "@/lib/serverApi";
 import Loader from "@/components/Loader/Loader";
+import TransactionsTotalAmount from "@/components/TransactionsTotalAmount/TransactionsTotalAmount";
+import css from "@/app/transactions/history/[type]/page.module.css";
 
 interface HistoryPageProps {
   params: Promise<{ type: string }>;
@@ -14,21 +14,27 @@ export default async function TransactionsHistoryPage({
   params,
 }: HistoryPageProps) {
   const { type } = await params;
-
   const transactionType = type === "incomes" ? "incomes" : "expenses";
-
   const data = await getTransactionByType(transactionType);
 
   return (
     <main className={css.container}>
-      <h1 className={css.title}>
-        {transactionType === "incomes" ? "All Incomes" : "All Expenses"}
-      </h1>
-      <p className={css.description}>
-        {transactionType === "incomes"
-          ? "Track and celebrate every bit of earnings effortlessly! Gain insights into your total revenue in a snap."
-          : "View and manage every transaction seamlessly! Your entire financial landscape, all in one place."}
-      </p>
+      <div className={css.topSection}>
+        <div className={css.textBlock}>
+          <h1 className={css.title}>
+            {transactionType === "incomes" ? "All Income" : "All Expense"}
+          </h1>
+          <p className={css.description}>
+            {transactionType === "incomes"
+              ? "Track and celebrate every bit of earnings effortlessly! Gain insights into your total revenue in a snap."
+              : "View and manage every transaction seamlessly! Your entire financial landscape, all in one place."}
+          </p>
+        </div>
+
+        <div className={css.totalAmount}>
+          <TransactionsTotalAmount />
+        </div>
+      </div>
 
       <div className={css.historyContent}>
         <TransactionsSearchTools />
@@ -40,3 +46,33 @@ export default async function TransactionsHistoryPage({
     </main>
   );
 }
+// export default async function TransactionsHistoryPage({
+//   params,
+// }: HistoryPageProps) {
+//   const { type } = await params;
+
+//   const transactionType = type === "incomes" ? "incomes" : "expenses";
+
+//   const data = await getTransactionByType(transactionType);
+
+//   return (
+//     <main className={css.container}>
+//       <h1 className={css.title}>
+//         {transactionType === "incomes" ? "All Incomes" : "All Expenses"}
+//       </h1>
+//       <p className={css.description}>
+//         {transactionType === "incomes"
+//           ? "Track and celebrate every bit of earnings effortlessly! Gain insights into your total revenue in a snap."
+//           : "View and manage every transaction seamlessly! Your entire financial landscape, all in one place."}
+//       </p>
+//       <TransactionsTotalAmount />
+//       <div className={css.historyContent}>
+//         <TransactionsSearchTools />
+
+//         <Suspense fallback={<Loader />}>
+//           <TransactionsList data={data} isLoading={false} />
+//         </Suspense>
+//       </div>
+//     </main>
+//   );
+// }
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import css from "./Header.module.css";
 import { useUserStore } from "@/store/useUserStore";
 import TransactionsHistoryNav from "../TransactionsHistoryNav/TransactionsHistoryNav";
@@ -9,12 +10,33 @@ import Logo from "../Logo/Logo";
 
 export default function Header() {
   const { isAuthenticated } = useUserStore();
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      setIsScrolled(window.scrollY > 0);
+    };
+
+    onScroll();
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
+  const headerClasses = `
+    ${css.header}
+    ${isAuthenticated ? css.authBorder : ""}
+    ${isScrolled ? css.scrolled : ""}
+  `;
+
+  const containerClasses = `
+    ${css.headerContainer}
+    ${!isAuthenticated ? css.isGuest : ""}
+  `;
 
   return (
-    <header className={css.header}>
-      <div
-        className={`${css.headerContainer} ${!isAuthenticated ? css.isGuest : ""}`}
-      >
+    <header className={headerClasses}>
+      <div className={containerClasses}>
         <div className={css.logoWrapper}>
           <Logo />
         </div>
@@ -29,6 +51,7 @@ export default function Header() {
                 <UserBarBtn />
               </div>
             </div>
+
             <div className={css.mobileNav}>
               <BurgerMenuBtn />
             </div>
